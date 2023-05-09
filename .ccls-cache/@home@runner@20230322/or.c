883efa4476022dce5782e1702c8e99db4036e160
verliebtchatonmw <128569707+verliebtchatonmw@users.noreply.github.com>
2023-05-09 05:43:48 +0000

    condition_myscore.c

---
+++ .ccls-cache/@home@runner@20230322/or.c
@@ -0,0 +1,9 @@
+// #include <stdio.h>
+// main()
+// {
+//     int a=15; // 01111
+//     int b=20; // 10100
+//     int c=a|b; // 11111 : 31
+
+//     printf("OR 연산 결과 : %d", c);
+// }
---
+++ condition_myscore.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+main(){
+  int x;
+  printf("성적을 입력하세요.\n");
+  scanf("%d", &x);
+  
+if(x>=90)
+  printf("A\n");
+else if(x>=80)
+  printf("B\n");
+else if(x>=70)
+  printf("C\n");
+else if(x>=60)
+  printf("D\n");
+else if(x>=50)
+  printf("E\n");
+else if(x<50)
+  printf("F\n");
+else
+  printf("오류입니다. 프로그램을 다시 실행하세요.\n");
+}
